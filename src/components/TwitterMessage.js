9da0e446d888ffc3e115daea10768a17736370de
Artur <info@aigroupusa.com>
2022-02-17 16:54:42 -0500

    commit tree d91a0b4de22043b69539f4cb25db8705210ece3e

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,15 +4,33 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: '',
+      maxChars: 280,
+    };
   }
+
+  handleChange = (event) => {
+    this.setState({
+      content: event.target.value,
+      maxChars: 279 - this.state.content.length
+    });
+  };
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
-      </div>
+        <input 
+          type="text" 
+          name="message" 
+          id="message"
+          onChange={(event) => this.handleChange(event)}
+          value={this.state.content} 
+          />
+            <h1>Remaining characters: {this.state.maxChars}</h1>
+          </div>
     );
   }
 }
